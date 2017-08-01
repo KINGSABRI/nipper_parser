736f0d6f9e30371047a6f6a84749ef7cecd6ce34
KING SABRI <king.sabri@gmail.com>
2017-08-01 23:52:41 +0300

    modifications

--- lib/nipper_parser.rb
+++ lib/nipper_parser.rb
@@ -1,5 +1,5 @@
-lib = File.expand_path('..', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+# lib = File.expand_path('..', __FILE__)
+# $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require 'nokogiri'
 require 'nipper_parser/version'
