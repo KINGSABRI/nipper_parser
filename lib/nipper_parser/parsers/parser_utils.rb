198bd5d85bfea86ffa47931777038a8eb83858a0
KING SABRI <king.sabri@gmail.com>
2017-08-01 22:22:36 +0300

    documentations

--- lib/nipper_parser/parsers/parser_utils.rb
+++ lib/nipper_parser/parsers/parser_utils.rb
@@ -3,7 +3,9 @@ module NipperParser
   # ParserUtils is a helper module for parsers' quick and dirty operations.
   module ParserUtils
     
-    # generate_table takes the table elements, parses it, then generate a [Hash]
+    # generate_table takes the table elements to parses it
+    # @param elements [Nokogiri::XML::Element]
+    # @return [Hash]
     def generate_table(elements)
       headers = elements[0].elements.map{|header| header.text.downcase.gsub(' ', '_').to_sym}
       body    = elements[1].elements.map{|e1| e1.elements.map{|e2| e2.text}}
