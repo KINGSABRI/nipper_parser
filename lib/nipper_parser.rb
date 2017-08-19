abea754fa1e5c5837560c00ffc617e17b8d49ca9
KING SABRI <king.sabri@gmail.com>
2017-08-19 22:35:21 +0300

    import filtering complexty and add usage

--- lib/nipper_parser.rb
+++ lib/nipper_parser.rb
@@ -7,6 +7,7 @@ require 'nipper_parser/parsers/parser_utils'
 require 'nipper_parser/parsers/information'
 require 'nipper_parser/parsers/security_audit'
 require 'nipper_parser/parsers/vulnerability_audit'
+require 'nipper_parser/parsers/filtering_complexity'
 
 module NipperParser
 
@@ -41,19 +42,19 @@ module NipperParser
     attr_reader :information
     # @!attribute #security_audit for security audit section, it calls [SecurityAudit] class
     attr_reader :security_audit
-    # @!attribute #vulnerability_audit for report general information, it calls [VulnerabilityAudit] class
+    # @!attribute #vulnerability_audit for report vulnerability audit section, it calls [VulnerabilityAudit] class
     attr_reader :vulnerability_audit
-    # @!attribute #stig_compliance for report general information, it calls [STIGCompliance] class
-    # attr_reader :stig_compliance
-
+    # @!attribute #filtering_complexity for report filtering complexity section, it calls [FilteringComplexity] class
+    attr_reader :filtering_complexity
 
     def initialize(file)
       config_parsed = Nokogiri::XML(File.open(file))
 
       # instantiate all parsers
-      @information          = Information.new(config_parsed)
-      @security_audit       = SecurityAudit.new(config_parsed)
-      @vulnerability_audit  = VulnerabilityAudit.new(config_parsed)
+      @information           = Information.new(config_parsed)
+      @security_audit        = SecurityAudit.new(config_parsed)
+      @vulnerability_audit   = VulnerabilityAudit.new(config_parsed)
+      @filtering_complexity  = FilteringComplexity.new(config_parsed)
     end
   end
 
@@ -67,16 +68,19 @@ if __FILE__ == $0
   pp nipper_parser.information.title
   pp nipper_parser.information.author
   pp nipper_parser.information.date
+  pp nipper_parser.security_audit.title
   pp nipper_parser.security_audit.introduction.security_issue_overview
   pp nipper_parser.security_audit.findings
-  pp nipper_parser.security_audit.findings[0].class
-  pp nipper_parser.security_audit.findings[0].title
-  pp nipper_parser.security_audit.findings[0].impact
-  pp nipper_parser.security_audit.conclusions
-  pp nipper_parser.security_audit.conclusions.per_device
-  pp nipper_parser.security_audit.recommendations.list
-  pp nipper_parser.security_audit.mitigation_classification
+  finding = nipper_parser.security_audit.findings[0]
+  pp finding.class
+  pp finding.title
+  pp finding.impact
+  pp finding.conclusions
+  pp finding.conclusions.per_device
+  pp finding.recommendations.list
+  pp finding.mitigation_classification
   pp nipper_parser.vulnerability_audit.class
+  pp nipper_parser.vulnerability_audit.title
   pp nipper_parser.vulnerability_audit.introduction
   pp nipper_parser.vulnerability_audit.introduction.excluded_devices
   cve = nipper_parser.vulnerability_audit.cves[0]
@@ -89,4 +93,23 @@ if __FILE__ == $0
   pp nipper_parser.vulnerability_audit.conclusions
   pp nipper_parser.vulnerability_audit.conclusions.list_critical
   pp nipper_parser.vulnerability_audit.recommendations
+  pp nipper_parser.filtering_complexity
+  pp nipper_parser.filtering_complexity.title
+  pp nipper_parser.filtering_complexity.introduction
+  pp nipper_parser.filtering_complexity.introduction.devices
+  observations = nipper_parser.filtering_complexity.observations
+  puts  "Number of observations: #{observations.size}"
+  observation = observations[2]
+  pp observation.title
+  pp observation.index
+  pp observation.ref
+  pp observation.overview
+  pp observation.affected_devices
+  pp observation.affected_devices[0].title
+  puts
+  pp observation.affected_devices[0].details
+  puts
+  pp observation.affected_devices[0].details_tables[0].title
+  pp observation.affected_devices[0].details_tables[0].tables
+
 end
